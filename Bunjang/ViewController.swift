e6e8a4cd1dabb06e67b52ee25526e71f43f1bd33
Mini <minnnidev@gmail.com>
2022-08-20 14:09:04 +0900

    Initial Commit

---
+++ Bunjang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bunjang
+//
+//  Created by 김민 on 2022/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
